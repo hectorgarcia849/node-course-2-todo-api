dc58e76f05007b2d85a42897367b9e51d6e28f5e
hector <hectorgarcia849@gmail.com>
2017-09-04 17:36:13 -0400

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,14 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+
+const todos = [{text: "First test todo"}, {text: "Second test todo"}];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
-}); //ensures db is empty
+    Todo.remove({})
+        .then(() => Todo.insertMany(todos))
+        .then(() => done())
+});
 
 describe('POST /todos', () => {
 
@@ -17,14 +22,14 @@ describe('POST /todos', () => {
             .send({text}) //allows us to send data with a request
             .expect(200) //status expected, can really have anything in here, just checks if it is included in response
             .expect((res) => {
-                expect(res.body.text).toBe(text); //checks if response includes the text we sent, b/c note in server.js we send the doc back as a response.
+                expect(res.body.text).toBe(text); //checks if response includes the text we sent, since in server.js we send the doc back as a response.
             })
             .end((err, res) => {
                 //possible errors, if expect != 200 or if res.body.text not text
                 if (err) {
                     return done(err);
                 }
-                Todo.find()
+                Todo.find({text})
                     .then((todos) => {
                         expect(todos.length).toBe(1);
                         expect(todos[0].text).toBe(text);
@@ -44,9 +49,21 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
            });
+    });
+});
+
+describe('GET /todos', ()=>{
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done); //not asynchronous
     });
 });
