9ccd3db8d925dd4b5ced61c81a0c4c11d3e03b23
hector <hectorgarcia849@gmail.com>
2017-09-04 20:15:44 -0400

    Mongoose connects uses process.env.MONGODB_URI and saved config vars in Heroku site

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,7 @@
 var  mongoose = require('mongoose');
-var {dbUser} = require('./../../dbInfo');
-var {dbPassword} = require('./../../dbInfo');
-
 
 mongoose.Promise = global.Promise; //need to let mongoose know that you want to use default promises that come with es6
-mongoose.connect(`mongodb://${dbUser}:${dbPassword}@ds123124.mlab.com:23124/node-course-todo` || 'mongodb://localhost:27017/TodoApp', {useMongoClient: true})
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useMongoClient: true})
     .then(() => console.log('Connected to Database'));
 
 module.exports = { mongoose };
