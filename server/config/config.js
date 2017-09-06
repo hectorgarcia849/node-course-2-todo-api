011913150ae4020461ffa4e4c6e59299f172a8ef
hector <hectorgarcia849@gmail.com>
2017-09-06 16:23:05 -0400

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,11 @@
 var env = process.env.NODE_ENV || 'development'; //allows us to define our environment variables. If in node, process.env.NODE_ENV will be set.  If in test, it will also be set, done in package.json in test.
 console.log('env *****', env);
 
-if(env === 'development'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if( env === 'test'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if(env === 'development' || env === 'test'){
+    var config = require('./config.json'); //json automatically parsed to object by require
+    var envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
